f248bfcc871084913cd769cfbd07953bbc339bfc
EnricoGeno <enricogeno@libero.it>
2025-04-09 16:08:18 +0200

    Milestone_2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const PoliticianCard = ({ name, image, country, position, years_in_office, biography }) => {
+  console.log('Rendered Card', name)
   return (
     <div className='card'>
       <img src={image} alt={name} />
@@ -15,14 +16,21 @@ const PoliticianCard = ({ name, image, country, position, years_in_office, biogr
 
 function App() {
 
-  const [politicians, setPoliticians] = useState([])
+  const [politicians, setPoliticians] = useState([]);
+  const [search, setSeach] = useState("");
 
   const loadPoliticians = async () => {
     const res = await fetch('https://boolean-spec-frontend.vercel.app/freetestapi/politicians');
     const data = await res.json();
-    console.log(data)
+    console.log(data);
     setPoliticians(data);
   }
+
+  const filteredPoliticians = politicians.filter(
+    ({ name, biography }) =>
+      name.toLowerCase().includes(search.toLowerCase()) ||
+      biography.toLowerCase().includes(search.toLowerCase())
+  );
 
   useEffect(() => {
     loadPoliticians()
@@ -30,7 +38,14 @@ function App() {
 
   return (
     <>
-      {politicians.map((p, i) => (
+      <label htmlFor="search">Search by name or biography:</label><br />
+      <input
+        type="text"
+        name='search'
+        value={search}
+        onChange={({ target }) => setSeach(target.value)}
+      />
+      {filteredPoliticians.map((p, i) => (
         <PoliticianCard key={i} {...p} />
       ))}
     </>
